b4b8c312b5fd4047305b663ced42c65062d5b5c9
gulshanmammadova <gulshanfm@code.edu.az>
2023-11-11 02:19:40 -0800

    QA

--- assets/main.js
+++ assets/main.js
@@ -1,153 +1,212 @@
-const apiKey = 'FZSRtaTpqQl54rMFau1Dg7o9rBgAgDnF';
-const apiUrl1 = 'https://api.currencybeacon.com/v1/convert';
-let inp1=document.querySelector('.inp1')
-let inp2=document.querySelector('.inp2')
-let oneTo=document.querySelector('span.one-to')
-let toValuta=document.querySelector('span.to-valuta')
-let oneFrom=document.querySelector('span.one-from')
-let oneTo2=document.querySelector('span.one-to2')
-let toValuta2=document.querySelector('span.to-valuta2')
-let oneFrom2=document.querySelector('span.one-from2')
-let b=document.querySelector('button.active')
-let baseCurrency = 'RUB'; 
-let targetCurrency = 'USD'; 
-// const amount = 100; 
+const apiKey = "FZSRtaTpqQl54rMFau1Dg7o9rBgAgDnF";
+const apiUrl1 = "https://api.currencybeacon.com/v1/convert";
+let inp1 = document.querySelector(".inp1");
+let inp2 = document.querySelector(".inp2");
+let oneTo = document.querySelector("span.one-to");
+let toValuta = document.querySelector("span.to-valuta");
+let oneFrom = document.querySelector("span.one-from");
+let oneTo2 = document.querySelector("span.one-to2");
+let toValuta2 = document.querySelector("span.to-valuta2");
+let oneFrom2 = document.querySelector("span.one-from2");
+let b = document.querySelector("button.active");
+let baseCurrency = "RUB";
+let targetCurrency = "USD";
+let block = document.querySelector(".have .d-none");
+let block2 = document.querySelector(".want-to-buy .d-none");
+
+// const amount = 100;
 let buttons = document.querySelectorAll(".btn-all-1 button");
 let buttons2 = document.querySelectorAll(".btn-all-2 button");
 
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-      buttons.forEach(btn => {
-          btn.classList.remove('active');
-      });
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
 
-      button.classList.add('active');
-      baseCurrency=button.innerHTML.trim();
-          inp1.value=''
-          inp2.value=''
-  });
-});
+    button.classList.add("active");
+    baseCurrency = button.innerHTML.trim();
+    inp1.value = 0;
+    inp2.value = 0;
+    const apiUrl = `${apiUrl1}?api_key=${apiKey}&from=${baseCurrency}&to=${targetCurrency}&amount=${1}`;
 
-buttons2.forEach(button => {
-  button.addEventListener('click', () => {
-      buttons2.forEach(btn => {
-          btn.classList.remove('active2');
-      });
-
-      button.classList.add('active2');
-      targetCurrency=button.innerHTML.trim();
-      console.log(targetCurrency)
-          inp1.value=''
-          inp2.value=''
-  });
-});
-inp2.addEventListener('change',async (e)=>{
-  if(parseFloat(inp2.value) === 0 ||inp2.value.trim() === ''){
-    inp1.value=0
-    oneFrom.innerHTML=baseCurrency;
-    oneTo.innerHTML=data1.value/inp2.target.value;
-    toValuta.innerHTML=targetCurrency;
-
-  }
-  else{
-    const newApiUrl = `${apiUrl1}?api_key=${apiKey}&from=${targetCurrency}&to=${baseCurrency}&amount=${e.target.value}`;
-
-    await fetch(`${newApiUrl}`)
-      .then(response => response.json())
-      .then(data21 => {
-        console.log(data21)
-
-        inp1.value=data21.value.toFixed(4)
-        const NewApiUrl1 = `${apiUrl1}?api_key=${apiKey}&from=${targetCurrency}&to=${baseCurrency}&amount=${1}`;
-
-         fetch(`${newApiUrl}`)
-          .then(response => response.json())
-          .then(data22 => {
-            oneFrom.innerHTML=baseCurrency;
-            oneTo.innerHTML=data22.value.toFixed(4);
-            toValuta.innerHTML=targetCurrency;
-           
-
-          })
-          .catch(error => {
-            console.error('Error:', error);
-          }); 
-          const apiUrl22 = `${apiUrl1}?api_key=${apiKey}&from=${baseCurrency}&to=${targetCurrency}&amount=${1}`;
-
-          fetch(`${apiUrl22}`)
-           .then(response => response.json())
-           .then(data221 => {
-            oneFrom2.innerHTML=targetCurrency;
-            oneTo2.innerHTML=data221.value.toFixed(4);
-            toValuta2.innerHTML=baseCurrency;
-           
-
-           })
-           .catch(error => {
-             console.error('Error:', error);
-           }); 
+    fetch(`${apiUrl}`)
+      .then((response) => response.json())
+      .then((datax) => {
+        oneFrom2.innerHTML = baseCurrency;
+        oneTo2.innerHTML = datax.value.toFixed(4);
+        toValuta2.innerHTML = targetCurrency;
       })
-      .catch(error => {
-        console.error('Error:', error);
-      }); 
-}
-})
-inp1.addEventListener('change',async (e)=>{
-   
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    const apiUrl2 = `${apiUrl1}?api_key=${apiKey}&from=${targetCurrency}&to=${baseCurrency}&amount=${1}`;
 
-  if(parseFloat(inp1.value) === 0 ||inp1.value.trim() === ''){
-    inp2.value=0
-    oneFrom.innerHTML=baseCurrency;
-    oneTo.innerHTML=data.value/inp1.target.value;
-    toValuta.innerHTML=targetCurrency;
+    fetch(`${apiUrl2}`)
+      .then((response) => response.json())
+      .then((data1) => {
+        oneFrom.innerHTML = targetCurrency;
+        oneTo.innerHTML = data1.value.toFixed(4);
+        toValuta.innerHTML = baseCurrency;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+});
 
-  }else{
+buttons2.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttons2.forEach((btn) => {
+      btn.classList.remove("active2");
+    });
+
+    button.classList.add("active2");
+    targetCurrency = button.innerHTML.trim();
+    console.log(targetCurrency);
+    inp1.value = 0;
+    inp2.value = 0;
+    const apiUrl = `${apiUrl1}?api_key=${apiKey}&from=${baseCurrency}&to=${targetCurrency}&amount=${1}`;
+
+    fetch(`${apiUrl}`)
+      .then((response) => response.json())
+      .then((datax) => {
+        oneFrom2.innerHTML = baseCurrency;
+        oneTo2.innerHTML = datax.value.toFixed(4);
+        toValuta2.innerHTML = targetCurrency;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    const apiUrl2 = `${apiUrl1}?api_key=${apiKey}&from=${targetCurrency}&to=${baseCurrency}&amount=${1}`;
+
+    fetch(`${apiUrl2}`)
+      .then((response) => response.json())
+      .then((data1) => {
+        oneFrom.innerHTML = targetCurrency;
+        oneTo.innerHTML = data1.value.toFixed(4);
+        toValuta.innerHTML = baseCurrency;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
+    
+  });
+
+
+const onChange1 = async (e) => {
+  block2.classList.remove("d-none");
+  block.classList.remove("d-none");
+
+  if (parseFloat(inp1.value) <= 0 || inp1.value.trim() === "") {
+    const apiUrlForZero = `${apiUrl1}?api_key=${apiKey}&from=${baseCurrency}&to=${targetCurrency}&amount=${1}`;
+    inp2.value = 0;
+
+    fetch(`${apiUrlForZero}`)
+      .then((response) => response.json())
+      .then((dataForZero) => {
+        oneFrom2.innerHTML = baseCurrency;
+        oneTo2.innerHTML = dataForZero.value.toFixed(4);
+        toValuta2.innerHTML = targetCurrency;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    const apiUrlForZero2 = `${apiUrl1}?api_key=${apiKey}&from=${targetCurrency}&to=${baseCurrency}&amount=${1}`;
+
+    fetch(`${apiUrlForZero2}`)
+      .then((response) => response.json())
+      .then((dataForZero2) => {
+        oneFrom.innerHTML = targetCurrency;
+        oneTo.innerHTML = dataForZero2.value.toFixed(4);
+        toValuta.innerHTML = baseCurrency;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  } else {
     const apiUrl = `${apiUrl1}?api_key=${apiKey}&from=${baseCurrency}&to=${targetCurrency}&amount=${e.target.value}`;
 
     await fetch(`${apiUrl}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-
-        inp2.value=data.value.toFixed(4)
-        const apiUrl = `${apiUrl1}?api_key=${apiKey}&from=${baseCurrency}&to=${targetCurrency}&amount=${1}`;
-
-         fetch(`${apiUrl}`)
-          .then(response => response.json())
-          .then(data => {
-            oneFrom2.innerHTML=baseCurrency;
-            oneTo2.innerHTML=data.value.toFixed(4);
-            toValuta2.innerHTML=targetCurrency;
-           
-
-          })
-          .catch(error => {
-            console.error('Error:', error);
-          }); 
-          const apiUrl2 = `${apiUrl1}?api_key=${apiKey}&from=${targetCurrency}&to=${baseCurrency}&amount=${1}`;
-
-          fetch(`${apiUrl2}`)
-           .then(response => response.json())
-           .then(data1 => {
-            oneFrom.innerHTML=targetCurrency;
-            oneTo.innerHTML=data1.value.toFixed(4);
-            toValuta.innerHTML=baseCurrency;
-           
-
-           })
-           .catch(error => {
-             console.error('Error:', error);
-           }); 
+      .then((response) => response.json())
+      .then((data) => {
+        inp2.value = data.value.toFixed(4);
       })
-      .catch(error => {
-        console.error('Error:', error);
-      }); 
-
-     
+      .catch((error) => {
+        alert("Sorry, there is a problem with the internet. Please try again.");
+      });
   }
+};
+const onChange2 = async (e) => {
+  block2.classList.remove("d-none");
+  block.classList.remove("d-none");
 
+  if (parseFloat(inp2.value) <= 0 || inp2.value.trim() === "") {
+    inp1.value = 0;
+    const apiUrlForZero2 = `${apiUrl1}?api_key=${apiKey}&from=${targetCurrency}&to=${baseCurrency}&amount=${1}`;
 
+    fetch(`${apiUrlForZero2}`)
+      .then((response) => response.json())
+      .then((dataForZero2) => {
+        oneFrom.innerHTML = baseCurrency;
+        oneTo.innerHTML = dataForZero2.value.toFixed(4);
+        toValuta.innerHTML = targetCurrency;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+    const apiUrlForZero21 = `${apiUrl1}?api_key=${apiKey}&from=${baseCurrency}&to=${targetCurrency}&amount=${1}`;
 
-})
+    fetch(`${apiUrlForZero21}`)
+      .then((response) => response.json())
+      .then((dataForZero2) => {
+        oneFrom2.innerHTML = targetCurrency;
+        oneTo2.innerHTML = dataForZero21.value.toFixed(4);
+        toValuta2.innerHTML = baseCurrency;
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  } else {
+    const apiUrlInp2 = `${apiUrl1}?api_key=${apiKey}&from=${targetCurrency}&to=${baseCurrency}&amount=${e.target.value}`;
 
-// 0 olanda islemir
+    await fetch(`${apiUrlInp2}`)
+      .then((response) => response.json())
+      .then((datai2) => {
+        inp1.value = datai2.value.toFixed(4);
+        const apiUrl21 = `${apiUrl1}?api_key=${apiKey}&from=${targetCurrency}&to=${baseCurrency}&amount=${1}`;
+
+        fetch(`${apiUrl21}`)
+          .then((response) => response.json())
+          .then((datax2) => {
+            oneFrom.innerHTML = baseCurrency;
+            oneTo.innerHTML = datax2.value.toFixed(4);
+            toValuta.innerHTML = targetCurrency;
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+        const apiUrlinp2 = `${apiUrl1}?api_key=${apiKey}&from=${baseCurrency}&to=${targetCurrency}&amount=${1}`;
+
+        fetch(`${apiUrlinp2}`)
+          .then((response) => response.json())
+          .then((datainp22) => {
+            oneFrom2.innerHTML = targetCurrency;
+            oneTo2.innerHTML = datainp22.value.toFixed(4);
+            toValuta2.innerHTML = baseCurrency;
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+      })
+      .catch((error) => {
+        alert("Sorry, there is a problem with the internet. Please try again.");
+      });
+  }
+};
+// 1cide sagda 0 yaz+
+
+//  defsultda asagini silmek duzdu?
+//button  deyisende asagida qalan evvelkinin sozleri+
+// 12 usd usd ye
